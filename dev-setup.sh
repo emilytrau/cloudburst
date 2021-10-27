2bcd11bcf093fcaada0e7363926a79b13d5d6c35
Angus Trau <me@angus.ws>
2021-10-27 11:06:37 +1100

    split up munge domains between clusters for security

--- dev-setup.sh
+++ dev-setup.sh
@@ -3,4 +3,6 @@
 docker-compose down --volumes --remove-orphans
 docker-compose build
 docker-compose run --no-deps --rm --entrypoint /opt/munge/sbin/mungekey cloud-slurmctl --verbose
+docker-compose run --no-deps --rm --entrypoint /opt/munge/sbin/mungekey static-slurmctl --verbose
+docker-compose run --no-deps --rm --entrypoint /opt/munge/sbin/mungekey slurmdb --verbose
 docker-compose run --no-deps --rm --entrypoint /usr/bin/pip cloud-slurmctl install -e /client
--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -5,4 +5,5 @@ echo "Waiting for database..."
 sleep 15
 
 /opt/munge/sbin/munged --verbose
+test -f /etc/munge-db/munge.key && /opt/munge/sbin/munged --verbose --key-file=/etc/munge-db/munge.key --socket=/opt/munge/var/run/munge/munge.socket.db
 exec "$@"
